7d48f477dc2091bc47459120ac09fe62382e6565
Priyabrata Mondal <priyabratamondal622@gmail.com>
2023-09-23 00:43:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,49 +1,4 @@
-/*
-import express from "express";
-import colors from "colors";
-import dotenv from "dotenv";
-import morgan from "morgan";
-import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoute.js";
-import categoryRoutes from "./routes/categoryRoutes.js";
-import productRoutes from "./routes/productRoutes.js";
-import cors from "cors";
 
-//configure env
-dotenv.config();
-
-//databse config
-connectDB();
-
-//rest object
-const app = express();
-
-//middelwares
-app.use(cors());
-app.use(express.json());
-app.use(morgan("dev"));
-
-//routes
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/category", categoryRoutes);
-app.use("/api/v1/product", productRoutes);
-
-//rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
-});
-
-//PORT
-const PORT = process.env.PORT || 8080;
-
-//run listen
-app.listen(PORT, () => {
-  console.log(
-    `Server Running at mode on port ${PORT}`.bgCyan
-      .white
-  );
-});
-*/
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
@@ -52,12 +7,12 @@ import connectDB from "./config/db.js";
 import authRoutes from './routes/authRoute.js';
 //import categoryRoutes from "./routes/categoryRoutes.js";
 //import productRoutes from "./routes/productRoutes.js";
-import categoryRoutes from "./routes/categoryRoutes.js"
-import productRoutes from "./routes/productRoutes.js"
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 
-import path from 'path'
-
+import path from 'path';
+import {fileURLToPath} from 'url';
 
 //configure env
 dotenv.config();
@@ -65,6 +20,9 @@ dotenv.config();
 
 //databse connection
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
